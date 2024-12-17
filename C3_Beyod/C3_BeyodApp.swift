8e1ef23655c2ab3cc52ef803d6fd64560a3c0759
Luis Mario Recinos Hernández <92188399+Mare1702@users.noreply.github.com>
2024-12-17 14:21:16 +0100

    Initial Commit

---
+++ C3_Beyod/C3_BeyodApp.swift
@@ -0,0 +1,17 @@
+//
+//  C3_BeyodApp.swift
+//  C3_Beyod
+//
+//  Created by Luis Mario Recinos Hernández  on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct C3_BeyodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C3_Beyod/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  C3_Beyod
+//
+//  Created by Luis Mario Recinos Hernández  on 17/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
